d0b18447dde34253106424b62a164ea7db1f104d
anaghalalu123 <anaghalalu777@gmail.com>
2025-04-05 15:51:28 -0400

    ATM SimulatorKotlin

---
+++ app/src/main/java/com/example/atmsimulator/ATMSimulator.kt
@@ -0,0 +1,59 @@
+package com.example.atmsimulator
+
+
+fun main() {
+
+    var balance = 1000.0 // initial balance
+    var isRunning = true
+
+    while (isRunning) {
+        println("\nWelcome to Simple ATM ")
+        println("1.Check balance")
+        println("2.Deposit")
+        println("3.Withdraw")
+        println("4.Exit")
+        println("5.Choose an option(1-4)")
+
+        when (readLine()) {
+
+            "1" -> {
+                println("Current balance: %.2f".format(balance))
+            }
+
+            "2" -> {
+                println("Enter amount to deposit")
+                var deposit = readLine()?.toDoubleOrNull()
+                if (deposit != null && deposit > 0) {
+                    balance += deposit
+                    println("$deposit deposited successfully")
+                } else {
+                    println("Invalid deposit amount")
+                }
+            }
+
+            "3" -> {
+                println("Enter the amount to withdraw")
+                val withdraw = readLine()?.toDoubleOrNull()
+                if (withdraw != null && withdraw > 0 && withdraw <= balance) {
+                    balance -= withdraw
+                    println("$withdraw withdrawn successfully")
+                } else {
+                    println("Invalid amount or insufficient balance")
+                }
+            }
+
+            "4" -> {
+                println("Thank you for choosing the atm. good bye!")
+                isRunning = false
+            }
+
+            else -> {
+                println("Invalid input, Try again")
+            }
+
+        }
+
+    }
+
+}
+
---
+++ app/src/main/java/com/example/atmsimulator/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.atmsimulator
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.atmsimulator.ui.theme.ATMSimulatorTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ATMSimulatorTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ATMSimulatorTheme {
+        Greeting("Android")
+    }
+}
